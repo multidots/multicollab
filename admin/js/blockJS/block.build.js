ac0f416f5251d193ce0df92275bdbcf44ae62bf9
Faisal Alvi <faisalalvi33@gmail.com>
2020-12-11 17:56:54 +0530

    introduced new way of toggle comments

--- admin/js/blockJS/block.build.js
+++ admin/js/blockJS/block.build.js
@@ -593,7 +593,7 @@ var $ = jQuery;
 $(window).on('load', function () {
 
     // Add history button.
-    var customButtons = '<div class="components-dropdown custom-buttons"><button type="button" aria-expanded="false" class="components-button has-icon" aria-label="Tools"><span class="dashicons dashicons-text-page" id="history-toggle"><img src="/wp-content/plugins/gutenberg_commenting/admin/images/commenting-logo.svg" width="18" alt="Comment Settings" /></span></button></div>';
+    const customButtons = '<div class="components-dropdown custom-buttons"><button type="button" aria-expanded="false" class="components-button has-icon" aria-label="Tools"><span class="dashicons dashicons-text-page" id="history-toggle"><img src="/wp-content/plugins/gutenberg_commenting/admin/images/commenting-logo.svg" width="18" alt="Comment Settings" /></span></button></div>';
 
     var loadAttempts = 0;
     var loadIcons = setInterval(function () {
@@ -607,7 +607,7 @@ $(window).on('load', function () {
         if (loadAttempts >= 3 && 1 === $('#history-toggle').length) {
             clearInterval(loadIcons);
         }
-    }, 2000);
+    }, 1000);
 
     var customHistoryPopup = '<div id="custom-history-popup"><div id="comments-toggle"><a href="javascript:void(0)">Hide All Comments</a></div><div id="custom-history-popup-inner"></div>';
     $('.edit-post-layout').append(customHistoryPopup);
@@ -19837,7 +19837,7 @@ var currentlyProcessingQueue;
   didWarnUpdateInsideUpdate = false;
   currentlyProcessingQueue = null;
 
-  
+
 }
 
 function createUpdateQueue(baseState) {
@@ -24152,7 +24152,7 @@ function insertNonHydratedInstance(returnFiber, fiber) {
               break;
 
             case SuspenseComponent:
-              
+
               break;
           }
 
