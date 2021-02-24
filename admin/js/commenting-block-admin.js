4c0255cf304653c4d13566454fc66be4bd12b2f2
rabiulislampatwary <rabiul.islam@multidots.com>
2021-02-24 15:44:59 +0600

    Improving assingable checkbox feature

--- admin/js/commenting-block-admin.js
+++ admin/js/commenting-block-admin.js
@@ -328,13 +328,34 @@
                 // Removing assignable checkbox if that user's email is not in the content or removed.
                 if( undefined !== typedText && typedText.length > 0 ) {
                     var assignCheckBoxId = `${currentBoardID}-cf-assign-to-user`;
-                    if( assignCheckBoxId.length > 0 ) {
-                        var assignCheckBoxUserEmail = $( assignCheckBoxId ).attr( 'data-user-email' );
-                        let checkEmailPattern       = new RegExp( assignCheckBoxUserEmail, 'igm' );
-                        let isThere                 = typedText.match( checkEmailPattern );
-                        if( ! isThere ) {
-                            $( assignCheckBoxId ).parent().remove();
+                    var emailSet = typedText.match( /[a-z0-9_\-\+\.]+@[a-z0-9\-]+\.([a-z]{2,4})(?:\.[a-z]{2})?/igm );
+                    emailSet = new Set( emailSet );
+                    var emailAddresses = Array.from( emailSet );
+                    if( null !== emailAddresses ) {
+                        if( assignCheckBoxId.length > 0 ) {
+                            var assignCheckBoxUserEmail = $( assignCheckBoxId ).attr( 'data-user-email' );
+                            let checkEmailPattern       = new RegExp( assignCheckBoxUserEmail, 'igm' );
+                            let isThere                 = typedText.match( checkEmailPattern );
+                            // isThere                     = new Set( isThere );
+                            // let usersEmailList          = Array.from( isThere );
+                            if( ! isThere ) {
+
+                                var appendInCheckbox = [];
+                                if( undefined !== cachedUsersList || null !== cachedUsersList ) {
+                                    cachedUsersList.forEach( function( item ) {
+                                        if( item.user_email === emailAddresses[0] ) {
+                                            appendInCheckbox.push( item );
+                                        }
+                                    } )
+                                }
+                                console.log( appendInCheckbox )
+                                $( assignCheckBoxId ).data( 'user-email', appendInCheckbox[0].user_email )
+                                $( assignCheckBoxId ).val( appendInCheckbox[0].ID );
+                                $( assignCheckBoxId ).next('i').html( `Assigned to ${appendInCheckbox[0].display_name}`  );
+                            }
                         }
+                    } else {
+                        $( assignCheckBoxId ).parent().remove();
                     }
 
                     // Remove assigner dom if there is not email in the editor.
