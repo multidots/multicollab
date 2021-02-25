2a29d166f2464534aa7d3252b1428400228057f1
rabiulislampatwary <rabiul.islam@multidots.com>
2021-02-25 11:14:03 +0600

    Documentation updated

--- admin/partials/commenting-block-settings-page.php
+++ admin/partials/commenting-block-settings-page.php
@@ -10,7 +10,7 @@ $cf_admin_notif = get_option( 'cf_admin_notif' );
 <div class="cf-plugin-settings">
     <div class="cf-plugin-header">
         <img src="<?php echo esc_url( COMMENTING_BLOCK_URL . '/admin/images/commenting-logo.svg' ); ?>"/>
-        <h1><?php esc_html_e( 'Multicollab - Editorial Commenting for WordPress', 'content-collaboration-inline-commenting' ); ?></h1>
+        <h1><?php esc_html_e( 'Multicollab - Google Doc-Style Editorial Commenting for WordPress', 'content-collaboration-inline-commenting' ); ?></h1>
     </div>
     <form id="cf-settings-form" method="post">
         <div class="cf-outer">
@@ -27,14 +27,14 @@ $cf_admin_notif = get_option( 'cf_admin_notif' );
                                     <p class="cf-greetings"><?php esc_html_e( 'Thanks for installing Google Doc-Style Editorial Commenting Plugin.', 'content-collaboration-inline-commenting' ) ?></p>
                                 <?php endif; ?>
                                 <div class="cf-video">
-                                    <iframe width="970" height="530" src="https://www.youtube-nocookie.com/embed/rDdgh_u8oVQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <iframe width="970" height="530" src="<?php echo esc_url( 'https://www.youtube-nocookie.com/embed/rDdgh_u8oVQ' ); ?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                                 </div>
                                 <div class="cf-video-links">
                                     <div class="cf-link-wrap">
-                                        <a href="https://www.multidots.com/google-doc-style-editorial-commenting-for-wordpress/?demo=start" class="cf-button button button-primary" target="_blank" id="cf-demo">Live Demo</a>
+                                        <a href="<?php echo esc_url( 'https://www.multidots.com/google-doc-style-editorial-commenting-for-wordpress/?demo=start' ); ?>" class="cf-button button button-primary" target="_blank" id="cf-demo"><?php esc_html_e( 'Live Demo', 'content-collaboration-inline-commenting' ); ?></a>
                                     </div>
                                     <div class="cf-link-wrap">
-                                        <p><a href="https://www.multidots.com/google-doc-style-editorial-commenting-for-wordpress/" class="cf-link" target="_blank" id="cf-demo"><?php esc_html_e( 'Learn More', 'content-collaboration-inline-commenting' ); ?></a> <?php esc_html_e( 'About Plugin', 'content-collaboration-inline-commenting' ); ?></p>
+                                        <p><a href="<?php echo esc_url( 'https://www.multidots.com/google-doc-style-editorial-commenting-for-wordpress/' ); ?>" class="cf-link" target="_blank" id="cf-demo"><?php esc_html_e( 'Learn More', 'content-collaboration-inline-commenting' ); ?></a> <?php esc_html_e( 'About Plugin', 'content-collaboration-inline-commenting' ); ?></p>
                                     </div>
                                 </div>
                             </div>
@@ -67,16 +67,16 @@ $cf_admin_notif = get_option( 'cf_admin_notif' );
             <div class="cf-right">
                 <div id="cf-developer" class="cf-card">
                     <div class="cf-card-header">
-                        <h3><?php esc_html_e( 'Plugin Developer'. 'content-collaboration-inline-commenting' ); ?></h3>
+                        <h3><?php esc_html_e( 'Plugin Developer', 'content-collaboration-inline-commenting' ); ?></h3>
                     </div>
                     <div class="cf-card-body">
                         <div class="cf-dev-img">
-                            <a href="https://www.multidots.com/" target="_blank">
+                            <a href="<?php echo esc_url( 'https://www.multidots.com/' ); ?>" target="_blank">
                                 <img src="<?php echo esc_url( COMMENTING_BLOCK_URL . '/admin/images/mdinc-logo.svg' ); ?>"/>
                             </a>
                         </div>
                         <p><?php esc_html_e( 'WordPress Development Experts & WordPress VIP Partner Agency', 'content-collaboration-inline-commenting' ); ?></p>
-                        <a href="https://www.multidots.com/" target="_blank" class="cf-button button button-primary"><?php esc_html_e( 'Visit Site', 'content-collaboration-inline-commenting' ); ?></a>
+                        <a href="<?php echo esc_url( 'https://www.multidots.com/' ); ?>" target="_blank" class="cf-button button button-primary"><?php esc_html_e( 'Visit Site', 'content-collaboration-inline-commenting' ); ?></a>
                     </div>
                 </div>
                 <div id="cf-contact" class="cf-card">
@@ -85,7 +85,7 @@ $cf_admin_notif = get_option( 'cf_admin_notif' );
                     </div>
                     <div class="cf-card-body">
                         <p><?php esc_html_e( 'For premium support and help with customizing this plugin for your business needs.', 'content-collaboration-inline-commenting' ) ?></p>
-                        <a href="https://www.multidots.com/google-doc-style-editorial-commenting-for-wordpress/#mdinc-contact-form-1" target="_blank" class="cf-button button button-primary"><?php esc_html_e( 'Contact Us', 'content-collaboration-inline-commenting' ); ?></a>
+                        <a href="<?php echo esc_url( 'https://www.multidots.com/google-doc-style-editorial-commenting-for-wordpress/#mdinc-contact-form-1' ); ?>" target="_blank" class="cf-button button button-primary"><?php esc_html_e( 'Contact Us', 'content-collaboration-inline-commenting' ); ?></a>
                     </div>
                 </div>
             </div>
