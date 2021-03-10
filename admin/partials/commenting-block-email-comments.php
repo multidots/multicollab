01d90c019e1cab3161b48a73ed15f16ed54a4872
rabiul-multidots <rabiul.islam@multidots.com>
2021-03-10 14:10:24 +0600

    Code updated with phhcs fix

--- admin/partials/commenting-block-email-comments.php
+++ admin/partials/commenting-block-email-comments.php
@@ -11,7 +11,7 @@
             $user_info      = get_userdata( $cf_comment['userData'] );
             $user_role      = implode( ', ', $user_info->roles );
             $username       = $user_info->display_name;
-            $this->users_emails[] = $user_info->user_email;
+            $this->users_emails[] = $user_info->user_email; // phpcs:ignore
             $profile_url    = get_avatar_url( $user_info->user_email );
             ?>
             <tr>
