de7f0ebd2c6a3c5a4d1c4fdc4251ecf363cf3782
rabiul-multidots <rabiul.islam@multidots.com>
2021-04-15 14:54:37 +0600

    Fixing url copy paste related issue

--- admin/assets/js/src/component/board.js
+++ admin/assets/js/src/component/board.js
@@ -151,7 +151,7 @@ export default class Board extends React.Component {
         var currentTextID = 'txt' + datatext;
         var newText       = $('#' + currentTextID).html();
         newText           = newText.replace( /<script[^>]*>(?:(?!<\/script>)[^])*<\/script>/gi, '' );
-        newText           = newText.replace( /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/ig, function( match ) {
+        newText           = newText.replace( /(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:\/~\+#]*[\w\-\@?^=%&amp;\/~\+#])?/ig, function( match ) {
             return `<a href="${match}" target="_blank">${match}</a>`;
         } );
 
